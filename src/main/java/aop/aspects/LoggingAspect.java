4e9b5faaa2c829e4943cce8ee066245ad35d2d8a
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-08-03 17:55:36 +0500

    пример @Aspect и @Before для логирования перед вызовом getBook

--- src/main/java/aop/aspects/LoggingAspect.java
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -1,7 +1,17 @@
 package aop.aspects;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
 @Component
 @Aspect
 public class LoggingAspect {
+    //advice - метод в аспекте который содержит сквозную логику. определяет
+    // что и когда должно происходить
+    //в скобках pointcut - когда должен выполниться сквозной код
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice() {
+        System.out.println("beforeGetBookAdvice: попытка получить книгу");
+    }
 }
