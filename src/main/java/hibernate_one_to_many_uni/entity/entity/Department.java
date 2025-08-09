78d43ab5d670d05c2516fbf3940bab9fb89ddc5d
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-08-09 19:29:57 +0500

    Примеры кода @OneToMany (создание связающей колонки происходит на стороне где many)

---
+++ src/main/java/hibernate_one_to_many_uni/entity/entity/Department.java
@@ -0,0 +1,87 @@
+package hibernate_one_to_many_uni.entity.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "max_salary")
+    private int maxSalary;
+    @Column(name = "min_salary")
+    private int minSalary;
+
+
+    // в случае с OneToMany внешний ключ будет на стороне таблицы в которой Many(в данном случае Employees)
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "department_id")
+    private List<Employee> employees;
+
+    public Department() {
+    }
+
+    public Department(String name, int maxSalary, int minSalary) {
+        this.name = name;
+        this.maxSalary = maxSalary;
+        this.minSalary = minSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployeeToDepartment(Employee employee){
+        if (this.employees == null){
+            this.employees = new ArrayList<>();
+        }
+
+        this.employees.add(employee);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", maxSalary=" + maxSalary +
+                ", minSalary=" + minSalary +
+                '}';
+    }
+}
---
+++ src/main/java/hibernate_one_to_many_uni/entity/entity/OneToMany.java
@@ -0,0 +1,54 @@
+package hibernate_one_to_many_uni.entity.entity;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class OneToMany {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+            session = factory.getCurrentSession();
+
+
+            //создание департамента с работниками
+            Department department = new Department("IT", 300, 1500);
+
+            Employee ivan = new Employee("Ivan", "Ivanov", 500);
+            Employee igor = new Employee("Igor", "Petrov", 1500);
+
+
+            department.addEmployeeToDepartment(ivan);
+            department.addEmployeeToDepartment(igor);
+            session.beginTransaction();
+            session.save(department);
+
+            System.out.println(department.getEmployees());
+
+            session.getTransaction().commit();
+
+
+        } catch (Exception e) {
+            System.out.println("exception : " + e);
+        } finally {
+            //Закрывать Session следует после каждого завершения работы с конкретной сессией
+            session.close();
+            //SessionFactory закрывается один раз при завершении приложения.
+            factory.close();
+        }
+    }
+
+
+}
+
+
+
+
